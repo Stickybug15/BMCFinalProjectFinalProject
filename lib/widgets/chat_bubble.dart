147aa53b722d0d1622e5f7a7a9c6aab9e90e9fbe
Stickybug15 <kiminonawa790@gmail.com>
2025-11-08 13:04:31 +0800

    Flutter 3.32.5 V Firebase Integration Final Project

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -1,71 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
-
   final String message;
-
   final bool isCurrentUser;
 
   const ChatBubble({
-
     super.key,
-
     required this.message,
-
     required this.isCurrentUser,
-
   });
 
   @override
-
   Widget build(BuildContext context) {
-
     return Container(
-
-      // 1. Align right if I'm the sender, left if I'm not
-
+      margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
       alignment: isCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
-
       child: Container(
-
-        constraints: BoxConstraints(
-
-          maxWidth: MediaQuery.of(context).size.width * 0.7,
-
-        ),
-
-        padding: const EdgeInsets.all(12),
-
-        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-
-        // 2. Different color for me vs. them
-
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
         decoration: BoxDecoration(
-
-          color: isCurrentUser ? Colors.deepPurple[400] : Colors.grey[200],
-
-          borderRadius: BorderRadius.circular(12),
-
+          color: isCurrentUser
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(16),
         ),
-
         child: Text(
-
           message,
-
           style: TextStyle(
-
-            color: isCurrentUser ? Colors.white : Colors.black87,
-
-            fontSize: 16,
-
+            color: isCurrentUser
+                ? Theme.of(context).colorScheme.onPrimary
+                : Theme.of(context).colorScheme.onSecondary,
           ),
-
         ),
-
       ),
-
     );
-
   }
-
 }
